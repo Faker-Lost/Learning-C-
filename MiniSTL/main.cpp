02971fa187d312f47fa17976c33c55066b7fe71a
faker <75052409+Faker-Lost@users.noreply.github.com>
2024-06-25 18:27:59 +0800

    Update main.cpp

--- MiniSTL/main.cpp
+++ MiniSTL/main.cpp
@@ -1,33 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include"miniList.h"
+// #define _CRT_SECURE_NO_WARNINGS
+// #include"miniList.h"
 
-void test1()
-{
-	List<int> ls;
-	ls.push_back(1);
-	ls.push_back(2);
-	ls.push_back(3);
-	ls.push_back(4);
+// void test1()
+// {
+// 	List<int> ls;
+// 	ls.push_back(1);
+// 	ls.push_back(2);
+// 	ls.push_back(3);
+// 	ls.push_back(4);
 
-	/*list<int>::const_iterator it = ls.begin();
-	while (it != ls.end())
-	{
-		cout << *it << " ";
-		it++;
-	}
-	cout << endl;*/
-	List<int>::iterator it = ls.begin();
-	while (it != ls.end())
-	{
-		cout << *it << " ";
-		it++;
-	}
-	cout << endl;
+// 	/*list<int>::const_iterator it = ls.begin();
+// 	while (it != ls.end())
+// 	{
+// 		cout << *it << " ";
+// 		it++;
+// 	}
+// 	cout << endl;*/
+// 	List<int>::iterator it = ls.begin();
+// 	while (it != ls.end())
+// 	{
+// 		cout << *it << " ";
+// 		it++;
+// 	}
+// 	cout << endl;
 
-}
-int main()
-{
-	test1();
+// }
+// int main()
+// {
+// 	test1();
 
-	return 0;
-}
+// 	return 0;
+// }
